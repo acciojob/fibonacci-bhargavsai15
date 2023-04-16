ab811bac356e5ae14d9b7f36eb4ca2a117053ccd
bhargavsai15 <36848893+bhargavsai15@users.noreply.github.com>
2023-04-16 23:31:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function fibonacci(num) {
-// your code here
+  // your code here
+    if(num<=1){
+      return num;
+    }
+    return fibonacci(num-1 )+fibonacci(num-2);
 }
 
 module.exports = fibonacci;
